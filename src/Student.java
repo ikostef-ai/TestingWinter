d5b9157d2d69a6a35e70eadbfb06b74678b1235a
ikostef-ai <ikonomov@shernet.sheridancollege.ca>
2026-01-20 11:39:03 -0500

    test version 2

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Stefan Jr
+ */
+public class Student {
+    private int sid;
+    
+}
